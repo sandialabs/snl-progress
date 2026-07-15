a5c8dfb7a2141c56bf9bd2f4afd52c184165a8cd
Eriel Cabrera <eecabre@sandia.gov>
2026-07-15 11:56:58 -0600

    fix DLL attemp

--- hooks/runtime_hook_casadi.py
+++ hooks/runtime_hook_casadi.py
@@ -2,9 +2,19 @@ import os
 import sys
 
 if sys.platform == "win32" and getattr(sys, "frozen", False):
-    bundle_dir = os.path.dirname(sys.executable)
+    bundle_dir = sys._MEIPASS
+    casadi_dir = os.path.join(bundle_dir, "casadi")
+
     if hasattr(os, "add_dll_directory"):
         os.add_dll_directory(bundle_dir)
+        if os.path.isdir(casadi_dir):
+            os.add_dll_directory(casadi_dir)
+
     path = os.environ.get("PATH", "")
-    if bundle_dir not in path:
-        os.environ["PATH"] = bundle_dir + os.pathsep + path
+    for d in [casadi_dir, bundle_dir]:
+        if d not in path:
+            path = d + os.pathsep + path
+    os.environ["PATH"] = path
+
+    if os.path.isdir(casadi_dir):
+        os.environ["CASADIPATH"] = casadi_dir
